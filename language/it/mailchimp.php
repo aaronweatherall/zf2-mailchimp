06bcbc92fb44f4d9b1a36f9906822e3adae36f20
Stefano Torresi <webdev@stefanotorresi.it>
2013-09-19 18:21:04 +0200

    strip useless docblocks

--- language/it/mailchimp.php
+++ language/it/mailchimp.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @author Stefano Torresi (http://stefanotorresi.it)
- * @license See the file LICENSE.txt for copying permission.
- * ************************************************
- */
 
 return array(
     'First Name' => 'Nome',
--- src/Mailchimp/Form/SubscriptionForm.php
+++ src/Mailchimp/Form/SubscriptionForm.php
@@ -1,10 +1,4 @@
 <?php
-/**
- *
- * @author Stefano Torresi (http://stefanotorresi.it)
- * @license See the file LICENSE.txt for copying permission.
- * ************************************************
- */
 
 namespace Mailchimp\Form;
 
