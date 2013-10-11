d44b7d34ae344975063507c4d464c959ff061580
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-11 10:46:06 +0200

    add missing copys

--- language/it/mailchimp.php
+++ language/it/mailchimp.php
@@ -6,4 +6,6 @@ return array(
     'Email Address' => 'Indirizzo email',
     'I accept the privacy policy and the terms of use' => 'Accetto i termini di utilizzo e la privacy policy',
     'Subscribe' => 'Iscriviti',
+    'Thank you!' => 'Grazie!',
+    'An email has been sent to confirm your subscription' => 'Vi è stata inviata una email per confermare la vostra registrazione',
 );
